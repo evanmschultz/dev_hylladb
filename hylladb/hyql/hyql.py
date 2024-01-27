3de5b9225cc9b827c994f7766fd20215fc1fd6a5
Evan <evanmschultz@gmail.com>
2024-01-27 01:01:32 -0700

    May finish idea of HyQL syntax. Need to test and build out more to know.

--- hylladb/hyql/hyql.py
+++ hylladb/hyql/hyql.py
@@ -1,18 +1,26 @@
-from abc import ABC
 import re
-from typing import Any, Literal, LiteralString, Protocol, Union, runtime_checkable
-from pydantic import BaseModel, Field, field_validator, model_validator
 from enum import Enum
+from typing import Any, LiteralString, Union
+
+from pydantic import BaseModel, Field, field_validator, model_validator
 
 
-# Define a Protocol as a type hint
+class BasicComparison(str, Enum):
+    """
+    This Enum defines the basic comparison operators used in HyQL.
 
+    It provides a set of operators specifically designed for basic comparison within query conditions and is meant to be
+    used as a guide when you are uncertain of the operator syntax of HyQL.
 
-class ConditionsEnum:
-    _is_base_comparison: bool = True
+    Members:
+        `EQUAL` (str): Represents the '==' operator for equality.
+        `NOT_EQUAL` (str): Represents the '!=' operator for inequality.
+        `GREATER_THAN` (str): Represents the '>' operator for greater than comparisons.
+        `LESS_THAN` (str): Represents the '<' operator for less than comparisons.
+        `GREATER_THAN_OR_EQUAL` (str): Represents the '>=' operator for greater than or equal to comparisons.
+        `LESS_THAN_OR_EQUAL` (str): Represents the '<=' operator for less than or equal to comparisons.
+    """
 
-
-class BasicComparison(ConditionsEnum, Enum):
     EQUAL = "=="
     NOT_EQUAL = "!="
     GREATER_THAN = ">"
@@ -21,14 +29,44 @@ class BasicComparison(ConditionsEnum, Enum):
     LESS_THAN_OR_EQUAL = "<="
 
 
-class StringOperators(ConditionsEnum, Enum):
+class StringOperators(str, Enum):
+    """
+    This Enum classifies string-based operators for use in HyQL queries.
+
+    It provides a set of operators specifically designed for string comparison and manipulation within query conditions
+    and is meant to be used as a guide when you are uncertain of the operator syntax of HyQL.
+
+    Members:
+        `CONTAINS` (str): Checks if a given substring is present within a string.
+        `STARTS_WITH` (str): Evaluates if a string starts with a specified substring.
+        `ENDS_WITH` (str): Determines if a string ends with a certain substring.
+        `MATCHES` (str): Used for regex pattern matching within a string.
+    """
+
     CONTAINS = "contains"
     STARTS_WITH = "starts_with"
     ENDS_WITH = "ends_with"
     MATCHES = "matches"
 
 
-class CollectionOperators(ConditionsEnum, Enum):
+class CollectionOperators(str, Enum):
+    """
+    This Enum identifies operators specific to collection-based queries in HyQL.
+
+    These operators are useful for performing various operations on collections like lists, sets, and dictionaries within
+    HyQL conditions. They are designed to enhance the flexibility and capability of collection handling in queries.
+
+    Members:
+        `IN` (str): Checks if an element is present within a collection.
+        `NOT_IN` (str): Determines if an element is not part of a collection.
+        `ANY` (str): Returns true if any element in a collection meets a certain condition.
+        `ALL` (str): Ensures that all elements in a collection satisfy a specified condition.
+        `NONE` (str): Verifies that no elements in a collection match a given condition.
+        `LENGTH_EQUAL` (str): Compares if the length of a collection is equal to a specific value.
+        `LENGTH_GREATER_THAN` (str): Checks if the length of a collection is greater than a certain value.
+        `LENGTH_LESS_THAN` (str): Assesses if the length of a collection is less than a specified value.
+    """
+
     IN = "in"
     NOT_IN = "not in"
     ANY = "any"
@@ -39,39 +77,140 @@ class CollectionOperators(ConditionsEnum, Enum):
     LENGTH_LESS_THAN = "length_lt"
 
 
-class LogicalOperators(ConditionsEnum, Enum):
+class LogicalOperators(str, Enum):
+    """
+    This Enum categorizes logical operators used in HyQL to combine or invert conditions.
+
+    Logical operators are fundamental in constructing complex logical expressions in query conditions, allowing for more nuanced and precise data retrieval.
+
+    Members:
+        `AND` (str): Logical AND operator, used to ensure all combined conditions are true.
+        `OR` (str): Logical OR operator, used when any of the combined conditions being true is sufficient.
+        `NOT` (str): Logical NOT operator, used to invert the result of a condition.
+    """
+
     AND = "and"
     OR = "or"
     NOT = "not"
 
 
-class IdentityTypeOperators(ConditionsEnum, Enum):
+class IdentityTypeOperators(str, Enum):
+    """
+    This Enum contains operators for identity and type checks in HyQL.
+
+    These operators are particularly useful for type validation and identity comparison, enhancing the robustness and specificity of query conditions.
+
+    Members:
+        `IS` (str): Checks if two references point to the same object.
+        `IS_NOT` (str): Determines if two references do not point to the same object.
+        `IS_INSTANCE` (str): Verifies if an object is an instance of a specified class or type.
+    """
+
     IS = "is"
     IS_NOT = "is not"
     IS_INSTANCE = "isinstance"
 
 
-class BitwiseOperators(ConditionsEnum, Enum):
-    BITWISE_AND = "&"
-    BITWISE_OR = "|"
-    BITWISE_XOR = "^"
-    BITWISE_NOT = "~"
+# class BitwiseOperators(str, Enum):
+#     BITWISE_AND = "&"
+#     BITWISE_OR = "|"
+#     BITWISE_XOR = "^"
+#     BITWISE_NOT = "~"
 
 
-class NumericOperators(ConditionsEnum, Enum):
+class NumericOperators(str, Enum):
+    """
+    This Enum represents operators tailored for numeric comparisons in HyQL.
+
+    They are essential for performing more intricate numeric condition checks, such as comparing absolute values, within HyQL queries.
+
+    Members:
+        `ABS_EQUAL` (str): Checks if the absolute value of a number is equal to a given value.
+        `ABS_GREATER_THAN` (str): Compares if the absolute value of a number is greater than a specified value.
+        `ABS_LESS_THAN` (str): Determines if the absolute value of a number is less than a certain value.
+    """
+
     ABS_EQUAL = "abs_eq"
     ABS_GREATER_THAN = "abs_gt"
     ABS_LESS_THAN = "abs_lt"
 
 
-class DateTimeOperators(ConditionsEnum, Enum):
+class DateTimeOperators(str, Enum):
+    """
+    This Enum encapsulates operators for date and time comparisons in HyQL.
+
+    These operators are vital for queries that involve date and time criteria, allowing precise temporal data filtering and comparison.
+
+    Members:
+        `DATE_EQUAL` (str): Checks if a date is exactly equal to a specified date.
+        `DATE_BEFORE` (str): Determines if a date occurs before a given date.
+        `DATE_AFTER` (str): Assesses if a date happens after a certain date.
+        `DATE_WITHIN` (str): Verifies if a date falls within a specified time frame.
+    """
+
     DATE_EQUAL = "date_eq"
     DATE_BEFORE = "date_before"
     DATE_AFTER = "date_after"
     DATE_WITHIN = "date_within"
 
 
-class LogicOperators(ConditionsEnum, Enum):
+class LogicOperators(str, Enum):
+    """
+    This Enum class provides a comprehensive collection of operators used in HyQL for various types of comparisons and
+    logical operations. It integrates basic comparison, string manipulation, collection handling, logical, identity
+    type, numeric, and date-time operators.
+
+    It is meant to be used as a guide when you are uncertain of the operator syntax of HyQL. If you want easier access to
+    operators of a specific type, you can import the following Enums: `BasicComparison`, `StringOperators`,
+    `CollectionOperators`, `LogicalOperators`, `IdentityTypeOperators`, `NumericOperators`, and `DateTimeOperators`.
+
+    Members:
+        # Basic Comparison Operators
+        `EQUAL` (str): Represents the '==' operator for equality comparison.
+        `NOT_EQUAL` (str): Represents the '!=' operator for inequality comparison.
+        `GREATER_THAN` (str): Represents the '>' operator for greater than comparisons.
+        `LESS_THAN` (str): Represents the '<' operator for less than comparisons.
+        `GREATER_THAN_OR_EQUAL` (str): Represents the '>=' operator for greater than or equal to comparisons.
+        `LESS_THAN_OR_EQUAL` (str): Represents the '<=' operator for less than or equal to comparisons.
+
+        # String Operators
+        `CONTAINS` (str): Checks if a given substring is present within a string.
+        `STARTS_WITH` (str): Evaluates if a string starts with a specified substring.
+        `ENDS_WITH` (str): Determines if a string ends with a certain substring.
+        `MATCHES` (str): Used for regex pattern matching within a string.
+
+        # Collection Operators
+        `IN` (str): Checks if an element is present within a collection.
+        `NOT_IN` (str): Determines if an element is not part of a collection.
+        `ANY` (str): Returns true if any element in a collection meets a certain condition.
+        `ALL` (str): Ensures that all elements in a collection satisfy a specified condition.
+        `NONE` (str): Verifies that no elements in a collection match a given condition.
+        `LENGTH_EQUAL` (str): Compares if the length of a collection is equal to a specific value.
+        `LENGTH_GREATER_THAN` (str): Checks if the length of a collection is greater than a certain value.
+        `LENGTH_LESS_THAN` (str): Assesses if the length of a collection is less than a specified value.
+
+        # Logical Operators
+        `AND` (str): Logical AND operator, used to ensure all combined conditions are true.
+        `OR` (str): Logical OR operator, used when any of the combined conditions being true is sufficient.
+        `NOT` (str): Logical NOT operator, used to invert the result of a condition.
+
+        # Identity Type Operators
+        `IS` (str): Checks if two references point to the same object.
+        `IS_NOT` (str): Determines if two references do not point to the same object.
+        `IS_INSTANCE` (str): Verifies if an object is an instance of a specified class or type.
+
+        # Numeric Operators
+        `ABS_EQUAL` (str): Checks if the absolute value of a number is equal to a given value.
+        `ABS_GREATER_THAN` (str): Compares if the absolute value of a number is greater than a specified value.
+        `ABS_LESS_THAN` (str): Determines if the absolute value of a number is less than a certain value.
+
+        # DateTime Operators
+        `DATE_EQUAL` (str): Checks if a date is exactly equal to a specified date.
+        `DATE_BEFORE` (str): Determines if a date occurs before a given date.
+        `DATE_AFTER` (str): Assesses if a date happens after a certain date.
+        `DATE_WITHIN` (str): Verifies if a date falls within a specified time frame.
+    """
+
     # Basic comparison operators
     EQUAL = "=="
     NOT_EQUAL = "!="
@@ -101,11 +240,11 @@ class LogicOperators(ConditionsEnum, Enum):
     IS = "is"
     IS_NOT = "is not"
     IS_INSTANCE = "isinstance"
-    # Bitwise operators
-    BITWISE_AND = "&"
-    BITWISE_OR = "|"
-    BITWISE_XOR = "^"
-    BITWISE_NOT = "~"
+    # # Bitwise operators
+    # BITWISE_AND = "&"
+    # BITWISE_OR = "|"
+    # BITWISE_XOR = "^"
+    # BITWISE_NOT = "~"
     # Numeric operators
     ABS_EQUAL = "abs_eq"
     ABS_GREATER_THAN = "abs_gt"
@@ -116,94 +255,19 @@ class LogicOperators(ConditionsEnum, Enum):
     DATE_AFTER = "date_after"
     DATE_WITHIN = "date_within"
 
-    @staticmethod
-    def allowed_operators() -> LiteralString:
-        return " | ".join(member.value for member in LogicOperators)
+    @classmethod
+    def is_valid_operator(cls, value: str) -> bool:
+        """
+        Checks if a given string is a valid operator in HyQL based on the members of the LogicOperators Enum.
 
+        Args:
+            `value` (str): The string to be checked.
 
-# class FilterConditions(BaseModel):
-#     conditions: list[str] | None = Field(
-#         default=None,
-#         description="HyQL filter conditions",
-#     )
+        Returns:
+            `bool`: True if the string is a valid operator, False otherwise.
+        """
+        return any(value == op for op in cls)
 
-#     @field_validator("conditions")
-#     def validate_condition(cls, values) -> list[str]:
-#         pattern: LiteralString = (
-#             r"^[A-Za-z0-9_]+\s(" + "|".join([op.value for op in Condition]) + r")\s.+$"
-#         )
-
-#         for i, condition in enumerate(values):
-#             if not re.match(pattern, condition):
-#                 allowed_operators: LiteralString = Condition.allowed_operators()
-#                 raise ValueError(
-#                     f"Invalid condition format at index {i}: {condition}. Allowed format: <`field`> < {allowed_operators} > <`value`>"
-#                 )
-#         return values
-
-
-# class SortCondition(BaseModel):
-#     key: str
-#     order: Literal["asc", "desc"] = Field(default="asc")
-
-
-# query_idea = {
-#     "checkout": [
-#         {
-#             "path": "path1.sub_path1",
-#             "checkout": ["sub_path1.field1", "sub_path1.field2"],
-#         },
-#         {
-#             "path": "path2",
-#             "checkout": ["*all"],
-#         },
-#     ],
-#     "filters": [
-#         {
-#             "condition": {
-#                 "left": "path1.field1",
-#                 "operator": "==",
-#                 "right": "value1",
-#             }
-#         },
-#         "AND",
-#         {
-#             "group": [
-#                 {
-#                     "condition": {
-#                         "left": "path1.field2",
-#                         "operator": ">",
-#                         "right": 100,
-#                     }
-#                 },
-#                 "OR",
-#                 {
-#                     "condition": {
-#                         "left": "path2.field3",
-#                         "operator": "<",
-#                         "right": "path1.field2",
-#                         "right_is_path": True,
-#                     }
-#                 },
-#             ]
-#         },
-#     ],
-#     "sort": [{"path": "path1.field1", "order": "asc"}],
-#     "limit": 10,
-#     "offset": 0,
-# }
-
-
-# class HyllaPath(BaseModel):
-#     path: str = Field(
-#         pattern=r"^[A-Za-z0-9_]+(\.[A-Za-z0-9_]+)*$", description="HyQL path: a dot separated path the dictionary key, shelf, or section, eg. 'section.shelf.dict_key'"
-#     )
-
-
-# class HyllaPath(BaseModel):
-#     path: str = Field(
-#         pattern=r"^[A-Za-z0-9_]+(\.[A-Za-z0-9_]+)*$", description="HyQL path: a dot separated path the dictionary key, shelf, or section, eg. 'section.shelf.dict_key'"
-#     )
 
 path_dict: dict = {
     "pattern": r"^[A-Za-z0-9]+(?:[._][A-Za-z0-9]+)*$",
@@ -211,10 +275,24 @@ path_dict: dict = {
 }
 
 
-def operator_validator(value) -> str:
-    if value not in LogicOperators.allowed_operators():
+def _operator_validator(value: str) -> str:
+    """
+    Validates if a given string is a valid operator in HyQL based on the members of the LogicOperators Enum.
+
+    Args:
+        `value` (str): The string to be checked.
+
+    Returns:
+        `str`: The string if it is a valid operator.
+
+    Raises:
+        `ValueError`: If the string is not a valid operator.
+    """
+    if not LogicOperators.is_valid_operator(value):
         raise ValueError(
-            f"Invalid operator '{value}'. Allowed operators: {LogicOperators.allowed_operators()}"
+            "\n    HyllaDB Error:\n"
+            "        ---->"
+            f"Invalid operator '{value}'. Allowed operators: {', '.join(op.value for op in LogicOperators)}\n"
         )
     return value
 
@@ -227,11 +305,7 @@ class Condition(BaseModel):
 
     @field_validator("operator", mode="before")
     def validate_operator(cls, value) -> str:
-        if value not in LogicOperators.allowed_operators() or not value:
-            raise ValueError(
-                f"Invalid operator '{value}'. Allowed operators: {LogicOperators.allowed_operators()}"
-            )
-        return value
+        return _operator_validator(value)
 
     @model_validator(mode="after")
     def validate_right(cls, data) -> Any:
@@ -239,11 +313,15 @@ class Condition(BaseModel):
             pattern: LiteralString = path_dict["pattern"]
             if not isinstance(data.right, str):
                 raise ValueError(
+                    "\n    HyllaDB Error:\n"
+                    "        ---->"
                     f"When the `right_is_path` flag is set to True, the `right` value must be a valid path string for HyllaDB."
-                    " Example format: 'section.shelf.dict_key'."
+                    " Example format: 'section.shelf.dict_key'.\n"
                 )
             if not re.match(pattern, str(data.right)):
                 raise ValueError(
+                    "\n    HyllaDB Error:\n"
+                    "        ---->"
                     f"The value `{data.right}` does not match the required pattern. Example format: 'section.shelf.dict_key'."
                 )
         return data
@@ -251,21 +329,6 @@ class Condition(BaseModel):
 
 class ConditionDict(BaseModel):
     condition: Condition
-
-
-class KeyBasedCondition(BaseModel):
-    left: str = Field(**path_dict)
-    operator: str
-    right: Any
-    # right_is_path: bool = False
-
-    @field_validator("operator")
-    def validate_operator(cls, value) -> str:
-        if value not in LogicOperators.allowed_operators():
-            raise ValueError(
-                f"Invalid operator '{value}'. Allowed operators: {LogicOperators.allowed_operators()}"
-            )
-        return value
 
 
 class Group(BaseModel):
--- main.py
+++ main.py
@@ -4,68 +4,10 @@ from hylladb.hyql import (
     Revise,
     Remove,
 )
+from hylladb.hyql.hyql import LogicOperators
 
 from rich import print
 
-# Create
-# create_query = Write(
-#     path="parent_section.child_section.shelf_name",
-#     data={"field1": "new value", "field2": {"subfield": "another value"}},
-# )
-
-# Read
-# query = Checkout(
-#     path="parent_section.child_section.shelf_name",
-#     checkout=["field1", "field2.subfield"],
-#     filter=FilterConditions(conditions=["field1 == 'value'"]),
-#     sort=[SortCondition(key="field1", order="asc")],
-#     limit=10,
-#     offset=0,
-# )
-
-# Update
-# update_query = Revise(
-#     path="parent_section.child_section.shelf_name",
-#     filters=FilterConditions(conditions=["field1 == 'old value'"]),
-#     data={"field1": "updated value"},
-# )
-
-# # Delete
-# delete_query = Remove(
-#     path="parent_section.child_section.shelf_name",
-#     filters=[
-#         FilterConditions(
-#             conditions=[
-#                 "field1 == 'value to delete'",
-#                 "field2 not in 'another value'",
-#             ]
-#         )
-#     ],
-# )
-
-# print(create_query, "\n")
-# print(query, "\n")
-# print(update_query, "\n")
-# print(delete_query)
-
-# from hylladb.model_generator.auto_generate_models import (
-#     generate_models,
-#     User,
-# )
-
-# generate_models(User)
-
-# from hylladb.generated_models import UserReturn_ID
-# from uuid import uuid4
-
-# print(UserReturn_ID(id=uuid4()))
-
-# print(
-#     "\n[bold][blue]Hylla[/blue][yellow]DB[/yellow] [red]Error:[/red][/bold]\n"
-#     "[red]----> [/red]"
-#     "The number of keys in the path map does not match the number of path maps. An error must have occurred"
-#     " while removing the path map or before. The database may be in an inconsistent state and corrupted.\n"
-# )
 
 checkout_idea = {
     "checkout": [
@@ -92,7 +34,7 @@ checkout_idea = {
                 {
                     "condition": {
                         "left": "path1.field2",
-                        "operator": ">",
+                        "operator": LogicOperators.GREATER_THAN,
                         "right": 100,
                     }
                 },
